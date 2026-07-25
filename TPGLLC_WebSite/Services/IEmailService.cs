c9c79c13ced8f95663ef7079246a7e08fc257ad2
Thomas Parlette <thomasparlette@gmail.com>
2026-07-25 14:28:54 -0400

    Update to new website

--- TPGLLC_WebSite/Services/IEmailService.cs
+++ TPGLLC_WebSite/Services/IEmailService.cs
@@ -4,5 +4,5 @@ namespace TPGLLC_WebSite.Services;
 
 public interface IEmailService
 {
-    Task SendContactMessageAsync(ContactMessage message, CancellationToken cancellationToken = default);
+    Task SendPendingAppointmentAsync(AppointmentRequest request, CancellationToken cancellationToken = default);
 }
